cb870e563fecabd3fea9e46a73e50eed79060477
susoroh1 <susoroh@venturseed.com>
2024-01-03 20:27:57 +0100

    changed base url

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://hairven-engine.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://hairven-engine.onrender.com/";
 
 
